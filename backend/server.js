4f472e05a339cba34646fb29344f9f4898f6d1bc
Ansh Mishra <anshmotivation@gmail.com>
2025-02-24 10:54:12 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,77 @@
+const express = require('express') // commonjs module syntax
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+const path = require('path')
+const PORT = process.env.PORT || 3000
+
+// Connect to database
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+console.log("MONGODB_URL:", process.env.MONGODB_URL); // Debugging line
+
+if (!process.env.MONGODB_URL) {
+    console.error("❌ Error: MONGODB_URL is missing from .env file");
+    process.exit(1);
+}
+
+
+const app = express()
+
+/**
+ * Each app.use(middleware) is called every time
+ * a request is sent to the server
+ */
+
+/**
+ * This is a built-in middleware function in Express.
+ * It parses incoming requests with JSON payloads and is based on body-parser.
+ */
+app.use(express.json())
+
+/**
+ * This is a built-in middleware function in Express.
+ * It parses incoming requests (Object as strings or arrays) with
+ * urlencoded payloads and is based on body-parser.
+ */
+app.use(express.urlencoded({ extended: false }))
+
+// Routes endpoints
+app.use('/api/users', require('./routes/userRoutes'))
+app.use('/api/tickets', require('./routes/ticketRoutes'))
+
+// Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+  // Set build folder as static folder
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+  })
+} else {
+  app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome to the Support Desk API' })
+  })
+}
+
+app.use(errorHandler)
+
+/**
+ * app.listen()
+ * Starts a UNIX socket and listens for connections on the given path.
+ * This method is identical to Node’s http.Server.listen().
+ */
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
